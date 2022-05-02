42fca895a3d2670eeb01ee36c091c1fcf297e1d9
Evanilson <programadornilson@gmail.com>
2022-05-02 01:53:08 -0300

    adicionando javascript

---
+++ script.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.max-width', {
+    duration: 3000
+})
+
+sr.reveal('.column', {
+    duration: 4000
+})
+sr.reveal('.sobre', {
+    duration: 4000
+})
+
+sr.reveal('.text', {
+    duration: 3000
+})
+
+sr.reveal('.container-1', {
+     rotate: {x:0, y:80, z:0},
+     duration: 4000 });
+
+sr.reveal('.projetos', {
+    rotate: {x:0, y:80, z:0},
+    duration: 4000
+});
+
+sr.reveal('.container', {duration: 5000})
+
+sr.reveal('.card', {
+    rotate: {x:0, y:80, z:0},
+    duration: 4000
+});
