ca2566778907538530ef97fe0894bd0a14c974af
Evanilson <programadornilson@gmail.com>
2022-05-03 15:24:41 -0300

    adicionando efeitos com javascript

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
+//Efeitos de Scroll
 window.sr = ScrollReveal({ reset: true });
 
 sr.reveal('.max-width', {
     duration: 2000
-})
+});
 
 sr.reveal('.column', {
     duration: 3000
-})
+});
 sr.reveal('.sobre', {
     duration: 3000
-})
+});
 
 sr.reveal('.text', {
     duration: 1000
-})
+});
 
 sr.reveal('.container-1', {
      rotate: {x:0, y:80, z:0},
@@ -30,3 +31,4 @@ sr.reveal('.card', {
     rotate: {x:0, y:80, z:0},
     duration: 3000
 });
+// Fim efeitos scroll
